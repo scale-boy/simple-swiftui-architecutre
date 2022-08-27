6be345c640e8205bd0174bf772a50f70c4a21118
tk1 <thuan2609.tk1@gmail.com>
2022-08-27 11:57:10 +0700

    [Task][01] Init Project

---
+++ swiftui-base/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftui-base
+//
+//  Created by Nguyen Van Thuan on 27/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
