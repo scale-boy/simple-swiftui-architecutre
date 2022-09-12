0be150ee159c6d04621dd8e94b12be284ee88b3e
tk1 <thuan2609.tk1@gmail.com>
2022-09-12 22:22:53 +0700

    [Task][03] Init First ContentView & Setup Version 0.1.0

---
+++ swiftui-base/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  swiftui-base
+//
+//  Created by Nguyen Van Thuan on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Thuan's First App")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
